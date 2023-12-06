33393ee184a93a251fb51ac7a437572b791df6fd
Bruno César <bcgmeireles@gmail.com>
2023-12-05 21:31:26 -0300

    feat: implement services

---
+++ servicos/src/app/cursos/cursos.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+import { CursosService } from './cursos.service';
+
+@Component({
+  selector: 'app-cursos',
+  templateUrl: './cursos.component.html',
+  styleUrl: './cursos.component.scss'
+})
+export class CursosComponent {
+
+  cursos: string[] = [];
+
+  cursosService: CursosService;
+
+  constructor() {
+    this.cursosService = new CursosService();
+  }
+
+  ngOnInit() {
+    this.cursos = this.cursosService.getCursos();
+  }
+
+}
