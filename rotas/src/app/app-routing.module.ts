557b6f2be9e16c59344d421feaa97930ef95ef41
Bruno César <bcgmeireles@gmail.com>
2023-12-29 10:45:16 -0300

    feat: setup routes

--- rotas/src/app/app-routing.module.ts
+++ rotas/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { MateriasComponent } from './materias/materias.component';
+
+const routes: Routes = [
+  { path: '', component:  HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'materias', component: MateriasComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
