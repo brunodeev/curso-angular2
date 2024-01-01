c2f50de61fd2cf12249fc72b28215ef9ffc11666
Bruno César <bcgmeireles@gmail.com>
2024-01-01 01:50:53 -0300

    feat: listen changes in router params

--- rotas/src/app/materia-detalhe/materia-detalhe.component.ts
+++ rotas/src/app/materia-detalhe/materia-detalhe.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-materia-detalhe',
@@ -9,9 +10,21 @@ import { ActivatedRoute } from '@angular/router';
 export class MateriaDetalheComponent {
 
   id?: string;
-
+  subscription?: Subscription;
+  
   constructor(private route: ActivatedRoute) {
-    this.id = this.route.snapshot.params['id'];
+    //this.id = this.route.snapshot.params['id'];
+  }
+
+  ngOnInit() {
+    this.subscription = this.route.params.subscribe(
+      (params: any) => {
+        this.id = params['id'];
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription!.unsubscribe();
   }
 
 }
