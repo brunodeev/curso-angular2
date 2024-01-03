088b22623029be237b5c6a6373504f903a85c428
Bruno César <bcgmeireles@gmail.com>
2024-01-03 08:54:17 -0300

    feat: maipulating query(url) params

--- rotas/src/app/materias/materias.component.ts
+++ rotas/src/app/materias/materias.component.ts
@@ -1,4 +1,7 @@
+import { ActivatedRoute, Router } from '@angular/router';
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+
 import { MateriasService } from './materias.service';
 
 @Component({
@@ -9,11 +12,25 @@ import { MateriasService } from './materias.service';
 export class MateriasComponent {
 
   materias?: any[];
+  pagina?: number;
+  subsciption?: Subscription
 
-  constructor(private service: MateriasService) {}
+  constructor(private service: MateriasService, private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit() {
-    this.materias = this.service.getMaterias(); 
+    this.materias = this.service.getMaterias();
+    this.subsciption = this.route.queryParams.subscribe((queryParams: any) => {
+      this.pagina = queryParams['pagina']
+    });
+  }
+
+  ngOnDestroy() {
+    this.subsciption?.unsubscribe();
+  }
+
+  nextPage() {
+    this.pagina!++;
+    this.router.navigate(['materias'], {queryParams: {'pagina': this.pagina!++}})
   }
 
 }
